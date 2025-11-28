fd333f1300bcbae86e0a830303aea52e8d817f0c
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-11-28 11:00:43 +0100

    Les: 1 - 2 -3: THREEjsJourney

--- src/script.js
+++ src/script.js
@@ -1,39 +1,32 @@
 import * as THREE from 'three'
 
+const sizes = {
+    width: 800,
+    height: 800,
+}
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1) // Vormt de box
+const  material = new THREE.MeshBasicMaterial({ color: 0xff000 }) // Texture (Kleur in dit geval)
+const mesh = new THREE.Mesh(geometry, material) // Maak het 3d object, box met texture
 scene.add(mesh)
 
-/**
- * Sizes
- */
-const sizes = {
-    width: 800,
-    height: 600
-}
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// Camera 
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // 75 = FOV
 camera.position.z = 3
 scene.add(camera)
 
-/**
- * Renderer
- */
+// Rendering
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
+
 renderer.render(scene, camera)
